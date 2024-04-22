d55f3704b0dfffd5be5d2e759179e3520a07773c
tuttigiuperterraa <105778039+tuttigiuperterraa@users.noreply.github.com>
2024-04-22 07:22:07 +0200

    Update philosophers.h

--- philosophers.h
+++ philosophers.h
@@ -8,11 +8,24 @@
 # include <sys/time.h>
 # include <limits.h>
 # include <stdbool.h>
+# include <errno.h>
 
 typedef struct s_fork t_fork;
 typedef struct s_philo t_philo;
 
-typedef	struct	s_param
+typedef	enum	e_code
+{
+	LOCK,
+	UNLOCK,
+	INIT,
+	DESTROY,
+	CREATE,
+	JOIN,
+	DETACH,
+
+} t_code;
+
+typedef	struct	s_params
 {
 	unsigned long	n_philos;
 	unsigned long	t_die;
@@ -21,6 +34,8 @@ typedef	struct	s_param
 	unsigned long	start_dinner;
 	unsigned long	*n_eat;
 	bool 			end;
+	bool			all_ready;
+	pthread_mutex_t table_mutex;
 	t_fork			*forks;
 	t_philo			*philos;
 }	t_params;
@@ -37,11 +52,17 @@ struct s_philo
 	int			pid;
 	pthread_t	tid;
 	long	n_eat;
-	t_fork	sx;
-	t_fork	dx;
+	t_fork	*first;
+	t_fork	*second;
+	t_params	*params;
+	bool	full;
+	bool	ready;
 	long	last_meal_time;
 };
 
 void    error_exit(char *msg);
+void *safe_malloc(size_t bytes);
+void    handle_mutex_error(int status, t_code opc);
+void safe_mutex(pthread_mutex_t *mutex, t_code opc);
 
 #endif
